933b0923de058050d1e3839242ab25db5bedf3c5
Kumar Utkarsh Das <116294098+Utkarshdas24@users.noreply.github.com>
2025-04-12 14:02:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,6 +177,8 @@ def camera_feed():
 @app.route("/live_count")
 def live_count():
     return jsonify({"current_count": len(deep_sort.tracker.tracks)})
-
+    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
